e3c9a0838f52dfd2eb94c3bb73a0934ea8840318
Julian Rubino <julian.rubino0@gmail.com>
2021-05-09 03:24:10 -0300

    Create WebSocket API routes and integrations

---
+++ wss_api_route_connect.tf
@@ -0,0 +1,30 @@
+### $connect route
+resource "aws_apigatewayv2_route" "websocket_api_connect_route" {
+  api_id    = aws_apigatewayv2_api.websocket_api.id
+  route_key = "$connect"
+  route_response_selection_expression = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.websocket_api_connect_integration.id}"
+}
+
+### $connect route response
+resource "aws_apigatewayv2_route_response" "websocket_api_connect_route_response" {
+  api_id             = aws_apigatewayv2_api.websocket_api.id
+  route_id           = aws_apigatewayv2_route.websocket_api_connect_route.id
+  route_response_key = "$default"
+}
+
+### $connect route integration
+resource "aws_apigatewayv2_integration" "websocket_api_connect_integration" {
+  api_id                    = aws_apigatewayv2_api.websocket_api.id
+  integration_type          = "AWS_PROXY"
+  content_handling_strategy = "CONVERT_TO_TEXT"
+  description               = "Lambda API integration for connectWebSocket function"
+  integration_uri           = aws_lambda_function.connect_route.invoke_arn
+}
+
+### $connect route integration response
+resource "aws_apigatewayv2_integration_response" "websocket_api_connect_integration_response" {
+  api_id                   = aws_apigatewayv2_api.websocket_api.id
+  integration_id           = aws_apigatewayv2_integration.websocket_api_connect_integration.id
+  integration_response_key = "/200/"
+}
